20763193073f896ffdf368250c3aef765aecae8c
Boronen <alisioboronen@gmail.com>
2026-03-29 23:12:25 +0200

    moduláris kiirás, több konstrukktorral

---
+++ src/csigaverseny/KiirasMod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csigaverseny;
+
+/**
+ *
+ * @author kovac
+ */
+public enum KiirasMod {
+    OSSZES,
+    ADOTT_KOR,
+    UTOLSO
+}
+
--- src/csigaverseny/Program.java
+++ src/csigaverseny/Program.java
@@ -3,7 +3,14 @@ package csigaverseny;
 public class Program {
     public static void main(String[] args) {
 
-        CsigaVerseny jatek = new CsigaVerseny(5);
+        // Minden kör kiírása
+         CsigaVerseny jatek = new CsigaVerseny(5);
+
+        // Csak a 3. kör kiírása
+        //CsigaVerseny jatek = new CsigaVerseny(5, 3);
+
+        // Csak az utolsó kör kiírása
+        //CsigaVerseny jatek = new CsigaVerseny(5, KiirasMod.UTOLSO);
 
         for (int i = 1; i <= jatek.getKorokSzama(); i++) {
             jatek.start(i);
